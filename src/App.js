b61a1b8eea7ae11531534951a756cc760bcb6ffe
Sammy <sammy.liaop@gmail.com>
2024-12-18 01:35:50 -0500

    should be working now

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello World!</p>
-      </header>
-    </div>
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/Name/')
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, [])
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Hello World!</p>
+        </header>
+      </div>
   );
 }
 
